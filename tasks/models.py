14c75463b01b51b053bf557dd32d4de4946adb46
Keranacardozo <silcardozo2000@gmail.com>
2024-09-08 16:41:51 -0400

    first commit

---
+++ tasks/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Task(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    status = models.CharField(max_length=50, choices=[('todo', 'To Do'), ('doing', 'Doing'), ('done', 'Done')])
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
